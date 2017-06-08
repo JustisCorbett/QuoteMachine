6dcbb42ec11180fe83ac8c0f04c796603a5efe04
Justis Corbett <corbettjustis@gmail.com>
2017-06-08 17:18:21 -0600

    twitter button again :(

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
 					url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
 					
 					dataType: 'json',
-					 success: function ( data ) {
-        		var post = data.shift(); // The data is an array of posts. Grab the first one.
-       			$( '#author' ).html("<p>&mdash; " + post.title + "</p>");
-       			$( '#quotes' ).html( post.content );
-						quote = $('#quotes').text;
-						author = $('#author').text;
+					  success: function ( data ) {
+        		  var post = data.shift(); // The data is an array of posts. Grab the first one.
+       			  $( '#author' ).html("<p>&mdash; " + post.title + "</p>");
+       			  $( '#quotes' ).html( post.content );
+						  quote = $('#quotes').innerText;
+						  author = $('#author').innerText;
 					 },
 					 cache: false
 				});
