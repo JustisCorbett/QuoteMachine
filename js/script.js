f0492bf45975f6da53f64838fe1c41fade453ecc
Justis Corbett <corbettjustis@gmail.com>
2017-06-08 15:06:45 -0600

    tried json instead of jsonp

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // JavaScript source code
 $(document).ready(function(){
     function getNewQuote(){
-        $.ajaxSetup({ cache: false });
-    		$.getJSON('https://crossorigin.me/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback', function(quoteResults) {
-        	document.getElementById('quotes').innerHTML = quoteResults[0].content;
-        	document.getElementById('author').innerHTML = "- " + quoteResults[0].title;
-        	tweetQuote();
-        
-    });
-			}
+        $.ajax({
+					url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+					jsonp: 'json',
+					dataType: 'json',
+					 success: function ( data ) {
+        		var post = data.shift(); // The data is an array of posts. Grab the first one.
+       			$( '#author' ).text( post.title );
+       			$( '#quotes' ).html( post.content );
+					 },
+					 cache: false
+				});
+			};
 
     getNewQuote();
 
