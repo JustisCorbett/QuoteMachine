6620ea064065134d1c844868c7e385dd2df7f270
Justis Corbett <corbettjustis@gmail.com>
2017-06-08 15:13:19 -0600

    removed redundant

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
     function getNewQuote(){
         $.ajax({
 					url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-					jsonp: 'json',
+					
 					dataType: 'json',
 					 success: function ( data ) {
         		var post = data.shift(); // The data is an array of posts. Grab the first one.
        			$( '#author' ).text( post.title );
-       			$( '#quotes' ).html( post.content );
+       			$( '#quotes' ).html("- " + post.content );
 					 },
 					 cache: false
 				});
