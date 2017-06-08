0481638cfa8e2273faabf4f24136520d4977d694
Justis Corbett <corbettjustis@gmail.com>
2017-06-07 21:40:54 -0600

    trying to use api but failing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+$(document).ready(function(){
+    function getNewQuote(){
+        $.ajaxSetup({ cache: false });
+    		$.getJSON('https://crossorigin.me/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback', function(quoteResults) {
+        	document.getElementById('quotes').innerHTML = quoteResults[0].content;
+        	document.getElementById('author').innerHTML = "- " + quoteResults[0].title;
+        	tweetQuote();
+        
+    });
+			}
+
+    getNewQuote();
+
+})
