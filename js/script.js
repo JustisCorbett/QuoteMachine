54ea7982acc0fffcc45e9210322ad088b5f379e2
Justis Corbett <corbettjustis@gmail.com>
2017-06-08 16:57:46 -0600

    please work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 $(document).ready(function(){
-		var quote = "";
-		var author = "";
+	var quote;
+	var author;
     function getNewQuote(){
         $.ajax({
 					url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
