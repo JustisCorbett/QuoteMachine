245bd7e8b3b1b3635add22442bbe452aab4d8209
Justis Corbett <corbettjustis@gmail.com>
2017-06-08 16:35:50 -0600

    retry tweet button

--- js/script.js
+++ js/script.js
@@ -21,6 +21,6 @@ $('#newQuote').on('click', function(event){
 });
 $('#tweetQuote').on('click', function(event){
 		event.preventDefault();
-		window.open("https://twitter.com/intent/tweet?text=" + '#quotes' + '#author' + " &hashtags=design");
+		window.open("https://twitter.com/intent/tweet?text=" + $( '#quotes' ).html + '#author' + " &hashtags=design");
 })
 })
